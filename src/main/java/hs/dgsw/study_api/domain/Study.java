8361a0a60b369b7de9ac47d0fea2f50cfd8cffbf
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-05-11 10:00:08 +0900

    first commit

---
+++ src/main/java/hs/dgsw/study_api/domain/Study.java
@@ -0,0 +1,38 @@
+package hs.dgsw.study_api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "study")
+public class Study {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "max_member", nullable = false)
+    private int maxMember;
+
+    @Builder
+    public Study(String title, String description, int maxMember) {
+        this.title = title;
+        this.description = description;
+        this.maxMember = maxMember;
+    }
+}
---
+++ src/test/java/hs/dgsw/study_api/StudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.dgsw.study_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
