ca7baaba713f83b3ae4ca1d20dcecfa62bbd242a
tlsalsco41122 <tlsalsco41122@gmail.com>
2026-05-12 12:34:30 +0900

    TokenAuthenticationFilter

---
+++ src/main/java/hs/dgsw/study_api/config/TokenAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package hs.dgsw.study_api.config;
+
+import hs.dgsw.study_api.config.jwt.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getAccessToken(request.getHeader(HEADER_AUTHORIZATION));
+
+        if(StringUtils.hasText(token) && tokenProvider.validToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(String authorization) {
+        if(authorization != null && authorization.startsWith(TOKEN_PREFIX)){
+            return authorization.substring(TOKEN_PREFIX.length()).trim();
+        }
+        return null;
+    }
+}
